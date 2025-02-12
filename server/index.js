563dde981d2f63e6b0cb1d0e9e94215806d737d8
saini <pulkit.saini@denefits.com>
2025-02-12 07:02:47 -0500

    fix

--- server/index.js
+++ server/index.js
@@ -1,54 +1,60 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require("@apollo/server/express4")
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const { default: axios } = require('axios');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const { USERS } = require("./user");
+const { TODOS } = require("./todo");
 
 async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
-            type User {
-                id: ID!
-                name: String!
-                userName: String!
-                email: String!
-                phone: String!
-            }
-                
-            type Todo{
-                id: ID!
-                title: String!
-                completed: Boolean
-                user: User
-            }
-            
-            type Query{
-                getTodos: [Todo]
-                getAllUsers: [User]
-                getUser(id:ID!): User
-            }
-        `,
+        type User {
+            id: ID!       
+            name: String!
+            username: String!
+            email: String!
+            phone: String!
+            website: String!
+        }
+
+        type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean
+            user: User
+        }
+
+        type Query {
+            getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id: ID!): User
+        }
+
+    `,
         resolvers: {
             Todo: {
-                user: async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data,
+                user: (todo) => USERS.find((e) => e.id === todo.id),
             },
             Query: {
-                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
-                getAllUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
-                getUser: async (parent, { id }) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,
-            }
-        }
+                getTodos: () => TODOS,
+                getAllUsers: () => USERS,
+                getUser: async (parent, { id }) => USERS.find((e) => e.id === id),
+            },
+        },
     });
-    app.use(bodyparser.json());
+
+    app.use(bodyParser.json());
     app.use(cors());
 
     await server.start();
+
     app.use("/graphql", expressMiddleware(server));
-    app.listen(8000, () => {
-        console.log('server started');
-    })
+
+    app.listen(8000, () => console.log("Serevr Started at PORT 8000"));
 }
 
-startServer()
+startServer();
